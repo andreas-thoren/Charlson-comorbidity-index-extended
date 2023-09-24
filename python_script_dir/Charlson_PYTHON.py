fe55d220db94b8e30dcc03bb2b3611146fdbd613
Andreas Thorén <andreasthoren79@gmail.com>
2023-09-24 13:44:17 +0200

    Started working on pseudoalgoritm

---
+++ python_script_dir/Charlson_PYTHON.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# TODO step-1: Import data from input file -> pd.DataFrame
+
+# TODO step-2: Adjust dataframe to specific format. Add columns for diagnos groups.
+
+# TODO step-3: Create secondary dataframe with diagnoses in one col,
+# icd type in second col and corresponding diagnos group in third.
+
+# TODO step-4: Use mask operation etc this is not finished
